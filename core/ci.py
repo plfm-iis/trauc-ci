99dacf5251107e6ae00602ba4281eebbd58d091b
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-03-21 17:43:28 +0800

    Update core

--- core/ci.py
+++ core/ci.py
@@ -6,6 +6,7 @@ import logging
 _base_dir = os.path.dirname(os.path.realpath(__file__))
 
 def run_sql(sql):
+    logging.info(sql)
     return os.popen("psql \
             -X \
             -t \
@@ -18,90 +19,80 @@ def run_sql(sql):
             --no-align \
             -c" + "\"" + sql + "\"").read()
 
+def update_sql(sql):
+    logging.info(sql)
+    return os.popen("psql \
+            -U pguser \
+            -d ci \
+            -c" + "\"" + sql + "\"").read()
+
 def get_targets():
     return run_sql("SELECT id FROM tools")
 
 def run_target(tid):
     [tname, cycle, command, repo_url, branch_name, days_to_run] = \
-            run_sql("SELECT \
-            name, \
-            test_cycle, \
-            command, \
-            repo_url, \
-            branch_name, \
-            days_to_run \
-            FROM tools WHERE id=" + tid).split(",")
+            run_sql("SELECT name, test_cycle, command, repo_url, branch_name, days_to_run FROM tools WHERE id=" + tid).split(",")
 
-    days_to_run = int(days_to_run) - 1
+    if len(days_to_run) == 0:
+        logging.info(update_sql("UPDATE tools SET days_to_run = 0 WHERE id=" + tid))
+    else:
+        days_to_run = int(days_to_run) - 1
+
     cycle = int(cycle)
-
     
     # Check the execution cycle
     if days_to_run == 0:
         days_to_run = cycle
         logging.info("Running ci for" + tname)
+        update_sql("UPDATE tools SET days_to_run = " + str(cycle) +" WHERE id=" + tid)
     else:
         logging.info(str(days_to_run) + "/" + str(cycle) + " days for " + tname)
+        update_sql("UPDATE tools SET days_to_run = " + str(cycle) +" WHERE id=" + tid)
         return
 
     # set commands
     if "z3" in tname:
-        cmd1 = "cd $SCRIPT_HOME && scripts/run_by_cron.sh " + \
-                tname + " " + z3_ubuntu + "Kaluza_unsat" + tid + " -"
-        cmd2 = "cd $SCRIPT_HOME && scripts/run_by_cron.sh " + \
-                tname + " " + z3_ubuntu + "PyEx_unsat" + tid + " -"
+        cmd1 = "cd $SCRIPT_HOME && ./scripts/run_by_cron.sh " + \
+                tname + " z3_ubuntu Kaluza_unsat " + tid + " -" + " > /dev/null 2&>1"
+        cmd2 = "cd $SCRIPT_HOME && ./scripts/run_by_cron.sh " + \
+                tname + " z3_ubuntu PyEx_unsat " + tid + " -" + " > /dev/null 2&>1"
     elif "cvc" in tname:
-        cmd1 = "cd $SCRIPT_HOME && scripts/run_by_cron.sh " + \
-                tname + " " + cvc4_ubuntu + "Kaluza_unsat" + tid + " -"
-        cmd2 = "cd $SCRIPT_HOME && scripts/run_by_cron.sh " + \
-                tname + " " + cvc4_ubuntu + "PyEx_unsat" + tid + " -"
+        cmd1 = "cd $SCRIPT_HOME && ./scripts/run_by_cron.sh " + \
+                tname + " cvc4_ubuntu Kaluza_unsat " + tid + " -" + " > /dev/null 2&>1"
+        cmd2 = "cd $SCRIPT_HOME && ./scripts/run_by_cron.sh " + \
+                tname + " cvc4_ubuntu PyEx_unsat " + tid + " -" + " > /dev/null 2&>1"
     else:
-        cmd1 = "cd $SCRIPT_HOME && scripts/run_z3_branch_by_cron.sh " + \
-                tname + "Kaluza_unsat" + tid
-        cmd2 = "cd $SCRIPT_HOME && scripts/run_z3_branch_by_cron.sh " + \
-                tname + "PyEx_unsat" + tid
+        cmd1 = "cd $SCRIPT_HOME && ./scripts/run_z3_branch_by_cron.sh " + \
+                tname + " Kaluza_unsat " + tid + " > /dev/null 2&>1"
+        cmd2 = "cd $SCRIPT_HOME && ./scripts/run_z3_branch_by_cron.sh " + \
+                tname + " PyEx_unsat " + tid + " > /dev/null 2&>1"
 
     # Execute
-    try:
-        pid1 = os.fork()
-    except:
-        logging.warning("fork1 error for " + tname)
-        return
-
-    if pid1 == 0:
-        # Child 1
-        logging.info(cmd1)
-        os.system(cmd1)
-        exit()
-    else:
-        # Parent after child1 forks
-        try:
-            pid2 = os.fork()
-            exit()
-        except:
-            logging.warning("fork2 error for " + tname)
-            return
-
-        if pid2 == 0:
-            # Child 2
-            logging.info(cmd2)
-            os.system(cmd2)
-
-    os.waitpid(0, 0)
-    os.waitpid(0, 0)
-    # After all child finish
+    os.popen(cmd1).read()
+    os.popen(cmd2).read()
 
 
 def main():
     os.environ["PGPASSWORD"] = os.environ["CI_DB_PASSWORD"]
     os.environ["SCRIPT_HOME"] = "/home/deploy/ci_scripts/"
-    os.environ["SCRIPT_HOME"] = "/home/deploy/output/"
-    logging.basicConfig(filename='/home/deploy/ci.log',level=logging.INFO,format='%(asctime)s %(message)s')
     targets = get_targets().splitlines()
 
+    children = []
     for target in targets:
-        run_target(target)
+        child = os.fork()
+        if child:
+            children.append(child)
+        else:
+            run_target(target)
+
+    for child in children:
+        os.waitpid(child, 0)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='/home/deploy/str_ci.log',\
+            level=logging.INFO,\
+            format='%(asctime)s %(message)s')
+
     main()
+    logging.info("=== All for today !!===")
