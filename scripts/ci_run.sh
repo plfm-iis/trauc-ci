e194c1b76137136911d02957ef9d23f6e5470869
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-04-12 14:41:49 +0800

    Set trace only to trauc

--- scripts/ci_run.sh
+++ scripts/ci_run.sh
@@ -12,10 +12,11 @@ python3.6 check_benchmark -c=$TARGET "${BENCHMARK_TARGET}/" > /dev/null
 PWD="deploy"
 sudo apt-get install -y -f sshpass
 
-sshpass -p "$PWD" scp -r trace deploy@10.32.0.252:/home/deploy/traces/${TARGET}.${DATE}.${BENCHMARK_TARGET}
-
 if [ "${COMMAND_NAME}" == "z3-trau" ]
     TARGET="z3trau"
+    chmod 777 -R trace/
+    sshpass -p "$PWD" scp -r trace deploy@10.32.0.252:/home/deploy/traces/${TARGET}.${DATE}.${BENCHMARK_TARGET}
+
 then
 fi
 cat ${BENCHMARK_TARGET}.${DATE}.${TARGET}.log
