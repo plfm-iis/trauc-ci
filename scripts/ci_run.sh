a1b37b0d19ed8f210daa28bd5893a08bf596acb9
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-04-12 09:49:46 +0800

    Fix z3-trau error

--- scripts/ci_run.sh
+++ scripts/ci_run.sh
@@ -8,6 +8,11 @@ cd ${BENCHMARK_PATH}/
 
 DATE=$(date "+%Y%m%d")
 python3.6 check_benchmark -c=$TARGET "${BENCHMARK_TARGET}/" > /dev/null
+
+if [ "${COMMAND_NAME}" == "z3-trau" ]
+    TARGET="z3trau"
+then
+fi
 cat ${BENCHMARK_TARGET}.${DATE}.${TARGET}.log
 echo "LOG.ERR:"
 cat ${BENCHMARK_TARGET}.${DATE}.${TARGET}.log.err
