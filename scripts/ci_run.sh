914b71656d90bbef47f00196457625e2dd87163c
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-04-12 14:51:35 +0800

    Fix bug

--- scripts/ci_run.sh
+++ scripts/ci_run.sh
@@ -9,16 +9,20 @@ cd ${BENCHMARK_PATH}/
 DATE=$(date "+%Y%m%d")
 python3.6 check_benchmark -c=$TARGET "${BENCHMARK_TARGET}/" > /dev/null
 
-PWD="deploy"
-sudo apt-get install -y -f sshpass
+if [ "${COMMAND_NAME}" == "trauc" ]
+then
+    PWD="deploy"
+    sudo apt-get install -y -f sshpass
+    chmod 777 -R trace/
+    sshpass -p "$PWD" scp -r trace deploy@10.32.0.252:/home/deploy/traces/${TARGET}.${DATE}.${BENCHMARK_TARGET}
+fi
 
 if [ "${COMMAND_NAME}" == "z3-trau" ]
     TARGET="z3trau"
-    chmod 777 -R trace/
-    sshpass -p "$PWD" scp -r trace deploy@10.32.0.252:/home/deploy/traces/${TARGET}.${DATE}.${BENCHMARK_TARGET}
 
 then
 fi
+
 cat ${BENCHMARK_TARGET}.${DATE}.${TARGET}.log
 echo "LOG.ERR:"
 cat ${BENCHMARK_TARGET}.${DATE}.${TARGET}.log.err
