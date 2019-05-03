6357d8786b5100619baa73c775fa010d453e5b7f
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-05-03 09:23:06 +0800

    Update cvc4 version

--- scripts/install_cvc4.sh
+++ scripts/install_cvc4.sh
@@ -7,7 +7,7 @@ set -e
 set -o pipefail
 
 # Install CVC4
-wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.6-x86_64-linux-opt \
+wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.7-x86_64-linux-opt \
         -O cvc4
 chmod +x cvc4
 sudo ln ${HOME}/cvc4 /usr/bin/cvc4
