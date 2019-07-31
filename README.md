91a26b68616b4bcc42e24c993af914b0cbab59e7
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-07-31 09:12:56 +0800

    Add sync_all.sh and Update READE.md

--- README.md
+++ README.md
@@ -3,13 +3,17 @@
 ## Installation
 Run `scripts/setup.sh` to build basic dockers.
 
-## Usage
+## Introductions
 The main entry point is **core/ci.py**.   
 This script is executed everyday by crontab.
 You can find this line in `/etc/crontab`
 ```
 12 1    * * *   deploy  cd /home/deploy/ci_scripts && ./core/ci.py > /home/deploy/ci_logs/$(date "+\%Y\%m\%d") 2>&1
 ```
+
+## Note
+- Use **./sync_all.sh <username> <password>** to update all servers' repo.
+- Update **servers** in **sync_all.sh** when a new vm is created.
 
 ## Logs
 Log for the cronjob is located in **$HOME/ci_logs**.   
---
+++ sync_all.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+####
+# Sync all servers' ci_script repo
+# ./sync_all.sh <username> <password>
+###
+servers=("10.32.1.8" "10.32.0.207" "10.32.0.207" "10.32.0.252" "10.32.1.7" "10.32.0.255" "10.32.0.222" "10.32.0.220" "10.32.0.107")
+
+
+function run_in_ssh() { # $1: ip address
+    ip=$1
+    username=$2
+    password=$3
+    echo $ip
+sshpass -p $password ssh deploy@${ip} /bin/bash << EOF
+    cd ci_scripts
+    git remote set-url origin https://github.com/plfm-iis/trauc-ci.git
+    git pull origin master
+EOF
+}
+
+
+
+username=$1
+password=$2
+if [ "$username" == "" ] || [ "$password" == "" ] 
+then
+    echo "Usage: ./sync_all.sh <username> <password>"
+    exit
+fi
+
+for _ip in "${servers[@]}"
+do
+    run_in_ssh $_ip $username $password
+done
