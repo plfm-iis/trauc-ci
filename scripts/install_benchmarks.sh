f18a952b94d223c907759700db9fe32f2cb6ad46
Hsin-Hung Lin <linil2j@gmail.com>
2019-11-12 18:21:24 +0800

    Fix a bug in script `install_benchmarks.sh`

--- scripts/install_benchmarks.sh
+++ scripts/install_benchmarks.sh
@@ -10,6 +10,6 @@ set -o pipefail
 git clone --branch master --depth 1 ${BENCHMARK_REPO} $BENCHMARK_PATH
 
 # Remove any logs happen to be existed
-rm -f ${BENCHMARK_PATH}/*log ${BENCHMARK_PATH}/*log.err ${BENCHMARK_PATH}/*.result ${BENCHMARK_PATH}/*.note
+rm -f ${BENCHMARK_PATH}/*.log ${BENCHMARK_PATH}/*.log.err ${BENCHMARK_PATH}/*.result ${BENCHMARK_PATH}/*.note
 
 cd $HOME
