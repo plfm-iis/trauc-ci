711c4bb50500bbc0a13129b75ceb0d7091a3a366
Hsin-Hung Lin <linil2j@gmail.com>
2020-04-24 20:12:47 +0800

    Remove g++-7 installation for z3-trau in `install_z3_branch.sh`

--- scripts/install_z3_branch.sh
+++ scripts/install_z3_branch.sh
@@ -12,12 +12,12 @@ set -e
 set -o pipefail
 
 # Change gcc to gcc-7 if z3-trau
-if [ "${COMMAND_NAME}" == "z3-trau" ]
-then
-    sudo update-alternatives --remove-all gcc
-    sudo apt-get install -y -f gcc-7 g++-7 
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
-fi
+#if [ "${COMMAND_NAME}" == "z3-trau" ]
+#then
+#    sudo update-alternatives --remove-all gcc
+#    sudo apt-get install -y -f gcc-7 g++-7
+#    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+#fi
 
 # Clone & Build Z3
 git clone -b $BRANCH $REPO_URL ${Z3_SRC_DIR}
